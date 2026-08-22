9d66f810f85b47049e3e7d11ae23c7910051eb4b
andr3348 <214445899+andr3348@users.noreply.github.com>
2026-08-22 18:25:41 -0500

    feat: implement GetTaskUseCase for retrieving a task by ID

---
+++ apps/api/src/modules/task/application/use-cases/get-task.use-case.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  type ITaskRepository,
+  TASK_REPOSITORY,
+} from '../../domain/repository/task.repository.interface';
+import { TaskEntity } from '../../domain/entity/task.entity';
+
+@Injectable()
+export class GetTaskUseCase {
+  constructor(
+    @Inject(TASK_REPOSITORY)
+    private readonly taskRepo: ITaskRepository,
+  ) {}
+
+  async execute(id: number): Promise<TaskEntity | null> {
+    return await this.taskRepo.findById(id);
+  }
+}
